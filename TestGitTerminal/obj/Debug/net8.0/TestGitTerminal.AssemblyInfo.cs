9ab5c897dcde9b4894b85dd7523d633d4fd2fd83
Carl hope <84421131+carlhope@users.noreply.github.com>
2024-11-06 13:15:39 +0000

    Add all files

--- TestGitTerminal/obj/Debug/net8.0/TestGitTerminal.AssemblyInfo.cs
+++ TestGitTerminal/obj/Debug/net8.0/TestGitTerminal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestGitTerminal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6535606755af1fc577f330f888894d8e57ca034d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestGitTerminal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestGitTerminal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
